975dbf1de507a346d9d72245757bf524ad70092e
Sean Lodhammar <contact@seanlodhammar.com>
2024-07-18 00:51:35 +0100

    Custom login and signup

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,30 +1,34 @@
-import { cookies } from 'next/headers';
-import { createUser } from '@/db/db';
-import { createSession } from '@/util/session';
-import type { NextRequest } from 'next/server';
+import { cookies } from "next/headers";
+import { createSession, getAuthSecret } from "@/util/session";
+import { user as userValidation } from "@/util/validation";
+import { ZodError } from "zod";
+import { constructErrorObj } from "@/util/validation";
 import * as jose from 'jose';
-import bcrypt from 'bcrypt';
-import { user as userValidation, constructErrorObj } from '@/util/validation';
-import { ZodError } from 'zod';
+import { getUserByEmail } from "@/db/db";
+import { compareSync } from 'bcrypt';
 import { randomBytes } from 'crypto';
 
-export const POST = async(req: NextRequest) => {
+export const POST = async(req: Request) => {
     const cookieStore = cookies();
     const data = await req.json();
 
     try {
         const parse = userValidation.parse({ email: data.email, password: data.password });
-
         const email = parse.email;
         const password = parse.password;
-    
-        const hash = bcrypt.hashSync(password, 12);
-        const user = await createUser(email, hash, 'custom');
 
-        if(!user) {
-            return Response.json({ errors: { email: 'Email already in use' }}, { status: 401 });
-        }
-    
+        const user = await getUserByEmail(email);
+
+        if(!user.password) throw new Error('User didn\'t signed up with different auth method');
+
+        const comparison = compareSync(password, user.password);
+
+        if(!comparison) {
+            return Response.json({
+                msg: 'Incorrect password',
+            }, { status: 401 });
+        };
+        
         const genSecret = randomBytes(16).toString('hex');
         const secret = new TextEncoder().encode(genSecret);
         const jwt = await new jose.SignJWT({ userId: user.id })  
@@ -34,19 +38,20 @@ export const POST = async(req: NextRequest) => {
             .setAudience('urn:example:audience')
             .setExpirationTime('12h')
             .sign(secret);
-        
-        cookieStore.set('auth-token', jwt, { maxAge: 12 * 60 * 60, path: '/', sameSite: 'lax' });
+
+        cookieStore.set('auth-token', jwt, { path: '/', maxAge: 12 * 60 * 60, sameSite: 'lax' });
         const sessionCreation = await createSession({ secret: genSecret, type: 'custom' });
-    
+
         if(!sessionCreation) {
             return Response.json({
                 msg: 'Something went wrong',
             }, { status: 400 })
         }
-    
+
         return Response.json({
             msg: 'Successful'
         }, { status: 200 });
+
     } catch (err) {
         if(err instanceof ZodError) {
             return Response.json({
@@ -54,8 +59,10 @@ export const POST = async(req: NextRequest) => {
                 errors: constructErrorObj(err),
             }, { status: 401 })
         }
-
+        console.log(err);
+        return Response.json({
+            msg: 'Something went wrong',
+        }, { status: 400 });
     }
-
 
 }

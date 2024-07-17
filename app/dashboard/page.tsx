624a98fb1d36ba3081c6a78aa97e537d32634e73
Sean Lodhammar <contact@seanlodhammar.com>
2024-07-18 00:55:32 +0100

    Placeholder dashboard code

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+const DashboardPage = () => {
+    return (
+        <main>
+            <h1>Dashboard</h1>
+        </main>
+    )
+}
+
+export default DashboardPage;

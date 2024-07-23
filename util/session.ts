5aaedfbd5d795cebf597758f28f1c14afc33bd31
Sean Lodhammar <contact@seanlodhammar.com>
2024-07-23 23:50:12 +0100

    Added custom session functions

--- util/session.ts
+++ util/session.ts
@@ -2,7 +2,7 @@ import 'server-only';
 import { cookies as cookies } from "next/headers";
 import { kv } from '@vercel/kv';
 
-export const createSession = async(data: { [key: string]: any }, temp?: boolean): Promise<{ sid: string; existing: boolean; data: any }> => {
+export const createSession = async(data: { [key: string]: any }, temp?: boolean) : Promise<{ sid: string; existing: boolean; data: any }> => {
     const cookieStore = cookies();
     const sidExist = cookieStore.get(!temp ? 'sid' : 'temp-sid');
     if(sidExist && sidExist.value) {
@@ -25,6 +25,20 @@ export const createSession = async(data: { [key: string]: any }, temp?: boolean)
     const expiry = (!temp ? 12 * 60 : 10) * 60;
     await kv.expire(`session-${sessionId}`, expiry);
     cookieStore.set(!temp ? 'sid' : 'temp-sid', sessionId, { maxAge: expiry, path: '/', sameSite: 'lax' });
+    return { sid: sessionId, existing: false, data: data };
+}
+
+export const createCustomSession = async(name: string, data: { [props: string]: string | number }, expiryInSeconds: number) => {
+    const cookieStore = cookies();
+    const sessionCookie = cookieStore.get(name);
+    if(sessionCookie && sessionCookie.value) {
+        const sessionData = await kv.hgetall(`session-${sessionCookie.value}`);
+        return { sid: sessionCookie.value, existing: true, data: sessionData };
+    }
+    const sessionId = crypto.randomUUID();
+    await kv.hset(`session-${sessionId}`, data);
+    await kv.expire(`session-${sessionId}`, expiryInSeconds);
+    cookieStore.set(name, sessionId, { sameSite: 'lax', maxAge: expiryInSeconds, path: '/' });
     return { sid: sessionId, existing: false, data: data };
 }
 
@@ -94,4 +108,29 @@ export const invalidateSession = async(temp?: boolean) => {
     const sid = sidCookie.value;
     await kv.del(`session-${sid}`);
     return true;
+}
+
+export const getCustomSession = async(name: string) => {
+    const cookieStore = cookies();
+    const cookie = cookieStore.get(name);
+    if(!cookie || !cookie.value) throw new Error('No cookie found', { cause: 'no-cookie' });
+    const session = await kv.hgetall(`session-${cookie.value}`);
+    if(!session) throw new Error('No session found', { cause: 'no-session' });
+    return session as { [props: string]: any };
+}
+
+export const setCustomKey = async(name: string, val: { [key: string]: any }) => {
+    const cookieStore = cookies() ;
+    const cookie = cookieStore.get(name);
+    if(!cookie || !cookie.value) throw new Error('No cookie found', { cause: 'no-cookie'});
+    await kv.hset(`session-${cookie.value}`, val);
+    return true;
+}
+
+export const invalidateCustomSession = async(name: string) => {
+    const cookieStore = cookies();
+    const cookie = cookieStore.get(name);
+    if(!cookie || !cookie.value) throw new Error('No cookie found', { cause: 'no-cookie' });
+    await kv.del(`session-${cookie.value}`);
+    return true; 
 }

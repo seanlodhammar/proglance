7594b1f5fc35e6676ee817654df4e4e3a93f9c3f
Sean Lodhammar <contact@seanlodhammar.com>
2024-07-23 23:48:38 +0100

    Added  function

--- util/auth.ts
+++ util/auth.ts
@@ -1,6 +1,7 @@
-import { getAuthSecret, setKey } from "./session";
-import { jwtVerify, decodeJwt} from 'jose';
+import { getAuthSecret, setKey, createSession } from "./session";
+import { jwtVerify, decodeJwt } from 'jose';
 import { JWTInvalid } from "jose/errors";
+import { getUserByEmail, createOAuthUser, updateUserRow } from "@/db/db";
 
 export const refresh = async(type: 'github' | 'google', refreshToken: string): Promise<string> => {
     if(!refreshToken) throw new Error('Invalid refresh token');
@@ -64,4 +65,50 @@ export const validateOauthAuthentication = async(session: Record<string, unknown
         }
     }
     throw new Error('Invalid type');
+}
+
+export const storeOAuthUser = async(code: string, type: 'github' | 'google'): Promise<void> => {
+    if(type === 'google') {
+        const encodedRedirectUri = encodeURIComponent(process.env.GOOGLE_CLIENT_CALLBACK as string);
+        const url = `https://oauth2.googleapis.com/token?code=${code}&client_id=${process.env.GOOGLE_CLIENT_ID}&client_secret=${process.env.GOOGLE_CLIENT_SECRET}&redirect_uri=${encodedRedirectUri}&grant_type=authorization_code`
+        const res = await fetch(url, { method: 'POST', headers: { 'Accept': 'application/json' } });
+        const tokens = await res.json()
+        const refreshToken = tokens.refresh_token;
+        const createdSession = await createSession({ type: 'google', 'user-credential': { refresh_token: refreshToken, id_token: tokens.id_token } });
+        if(!createdSession) throw new Error('No session created');
+        const user = decodeJwt(tokens.id_token);
+        console.log(user);
+        const email = user.email;
+        if(typeof email !== 'string') throw new Error('Invalid email');
+        const exist = await getUserByEmail(email, 'google');
+        if(!exist) {
+            await createOAuthUser(email, tokens.refresh_token, 'google');
+        } else {
+            if(exist.authType !== 'google') throw new Error('Email already exists', { cause: 'existing email' });
+            if(refreshToken !== exist.refresh) {
+                await updateUserRow(exist.id, { refresh: refreshToken });
+            }
+        }
+    } else if(type === 'github') {
+        if(!code) throw new Error('No code');
+        const access = await fetch(`https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`, { headers: { 'Accept': 'application/json' } });
+        const credentials = await access.json();
+        if(!credentials['access_token'] || !credentials['refresh_token']) throw new Error('No credentials');
+        const createdSession = await createSession({ 'user-credential': credentials, type: 'github' });
+        if(!createdSession) throw new Error('No session created');
+        const get = await fetch('https://api.github.com/user', { headers: { 'Authorization': `Bearer ${credentials.access_token}`, 'Accept': 'application/json' } });
+        const user = await get.json();
+        const email = user.email;
+        const refreshToken = credentials.refresh_token;
+        if(typeof email !== 'string') throw new Error('Invalid email');
+        const exist = await getUserByEmail(user.email);
+        if(!exist) {
+            await createOAuthUser(email, credentials.refresh_token, 'github');
+        } else {
+            if(exist.authType !== 'github') throw new Error('Email already exists', { cause: 'existing email' });
+            if(refreshToken !== exist.refresh) {
+                await updateUserRow(exist.id, { refresh: refreshToken });
+            }
+        }
+    }
 }

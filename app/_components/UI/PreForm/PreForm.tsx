a51c2e31e15a840b5745d47c8b3ed191d6a14347
Sean Lodhammar <contact@seanlodhammar.com>
2024-07-28 00:23:51 +0100

    Added `type` prop for login/signup links

--- app/_components/UI/PreForm/PreForm.tsx
+++ app/_components/UI/PreForm/PreForm.tsx
@@ -2,11 +2,11 @@ import styles from './PreForm.module.css';
 import type { ReactNode } from 'react';
 import { RectStack } from '../icons';
 
-const PreForm = ({ title, children }: { title: string; children: ReactNode }) => {
+const PreForm = ({ title, type, children }: { title: string; type?: 'auth' | 'generic'; children: ReactNode }) => {
     return (
         <div className={styles['wrapper']}>
             <RectStack className={styles['icon']} />
-            <h2 className={styles['heading']}>{ title }</h2>
+            <h2 className={`${styles['heading']} ${type === 'auth' ? styles['auth'] : ''}`}>{ title }</h2>
             { children }
         </div>
     )
